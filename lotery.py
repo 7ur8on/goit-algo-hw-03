19a4ad07066e07e35ab0bb9dcd70fe9f4f0d8b39
7ur8on <161379697+7ur8on@users.noreply.github.com>
2024-04-21 22:11:19 +0200

    Add files via upload

--- lotery.py
+++ lotery.py
@@ -1,10 +1,16 @@
 import random
-my_list = set() # створюємо пусту множину
+
 def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or quantity < min or quantity > max: #  обмеження для параметрів
+    if min < 1 or max > 1000 or min >= max or quantity < 1 or quantity > (max - min + 1): #  обмеження для параметрів
+        my_list = list() # створюємо пустий список
         return list(my_list)  # повертаємо пустий список
-    while len(my_list) < quantity:  # запускаєм цикл до певного параметру
-        random_number = random.randint(min, max)    # генеруєм випадкове число
-        my_list.add(random_number)   # як-що число не повторюється то додаєм в множину
-    return list(my_list)    # повертаємо і перетворюємо у список
+    sample_numbers = sorted(random.sample(range(min, max + 1), quantity)) # створюємо список з унікальними значеннями
+    return sample_numbers    # повертаємо і перетворюємо у список
 print(get_numbers_ticket(1, 1000, 5))   # викликаємо функцію з параметрами
+print(get_numbers_ticket(0, 50, 5))  # має вивести пустий список
+print(get_numbers_ticket(1, 1001, 10))  # має вивести пустий список
+print(get_numbers_ticket(50, 50, 5))  # має вивести пустий список
+print(get_numbers_ticket(1, 50, 0))  # має вивести пустий список
+print(get_numbers_ticket(1, 50, 51))  # має вивести пустий список
+print(get_numbers_ticket(10, 20, 15))  # має вивести пустий список
+print(get_numbers_ticket(1, 10, 6))  # повертає відсортований список унікальних значень
